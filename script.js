f502e5c993255fa9e7ed81d150cf6880ea0294f9
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-22 19:29:49 -0300

    Write the html for the modal and style it

---
+++ script.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  this.id = randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, status) {
+  const book = new Book(title, author, pages, status);
+  myLibrary.push(book);
+}
